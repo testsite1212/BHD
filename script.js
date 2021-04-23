b1e67708744a1b66ddedbc38cd45e2d71a888d8a
testsite1212 <82283132+testsite1212@users.noreply.github.com>
2021-04-23 13:24:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+let input = document.querySelector('input');
+let submit = document.querySelector('.submit');
+
+document('input') = function {
+	if (input = admin => {
+		window.open('https://instagram.com');
+})		
+}
